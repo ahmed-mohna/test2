b2fe7f003c968f995b6f167cecda77901f979a3c
ahmed mohna <ahmedmohna995@gmail.com>
2022-03-04 18:18:44 +0300

    Initial Commit

---
+++ test12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test12
+//
+//  Created by Ahmed Mohna on 04/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test12/test12App.swift
@@ -0,0 +1,17 @@
+//
+//  test12App.swift
+//  test12
+//
+//  Created by Ahmed Mohna on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct test12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
